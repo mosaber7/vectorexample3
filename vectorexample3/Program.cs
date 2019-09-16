f79190210eae0067bddcfde3eae02632fdd3908b
Mohamed Fares <mosaber7@users.campus>
2019-09-16 16:20:06 -0500

    Add project files.

---
+++ vectorexample3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using VectorMath;
+
+namespace vectorexample3
+{
+    class Program
+    {
+
+        Vector3 vec = new Vector3();
+        static void Main(string[] args)
+        {
+            Vector3 vec = new Vector3();
+            Console.WriteLine("Hello World!");
+            vec.add(new Vector3(1, 2, 3));
+        }
+    }
+}
